4c948578246e463eb0a02b1c810cc037dd4db379
dakone22 <35692622+dakone22@users.noreply.github.com>
2021-07-24 20:24:12 +0300

    Clean up

--- main.py
+++ main.py
@@ -10,38 +10,22 @@ from telethon.sessions import StringSession
 from telethon.tl.functions.channels import JoinChannelRequest
 from telethon.tl.types import Channel
 
+from kafka import KafkaProducer
+
 logging.basicConfig(format="%(asctime)s - %(name)s - %(levelname)s - %(message)s")
 logger = logging.getLogger('telegram-worker')
 logger.setLevel(logging.DEBUG)
 
-DEBUG = bool(os.environ.get('DEBUG', True))
-
-if DEBUG:
-    def send_message_data(message_data):
-        OUTPUT_FILENAME = "output.json"
-        if os.path.exists(OUTPUT_FILENAME):
-            with open(OUTPUT_FILENAME, "r", encoding="utf-8") as f:
-                messages = json.load(f)
-        else:
-            messages = []
-
-        messages.append(message_data)
-
-        with open("output.json", "w", encoding="utf-8") as output:
-            json.dump(messages, output, indent=4)
-else:
-    from kafka import KafkaProducer
-
-    producer = KafkaProducer(security_protocol="SSL", bootstrap_servers=os.environ.get("KAFKA_HOST"))
+producer = KafkaProducer(bootstrap_servers=os.environ.get("KAFKA_HOST"))
 
 
-    def send_message_data(message_data):
-        j = json.dumps(message_data)
-        logger.debug(f"Sending message to Kafka: {j}")
-        producer.send(j)
+def send_message_data(message_data):
+    j = json.dumps(message_data)
+    logger.debug(f"Sending message to Kafka: {j}")
+    producer.send('topic', j.encode('utf-8'))
 
 
-async def main(client):
+async def start(client):
     logger.info("Started")
 
     CHATS_TO_LISTEN = [
@@ -55,9 +39,10 @@ async def main(client):
         try:
             channel = await client.get_entity(chat)
             if not isinstance(channel, Channel):
-                logger.info(f"{chat} is not channel, skipping...")
+                logger.warning(f"Chat {chat} is not channel, skipping...")
                 continue
 
+            # TODO: ignore already joined
             await client(JoinChannelRequest(channel))
             logger.info(f"Joined to {channel}")
         except Exception:
@@ -78,26 +63,17 @@ async def main(client):
         send_message_data(message_data)
 
 
-if __name__ == '__main__':
-    if DEBUG:
-        logger.debug("Authentication from auth.json")
-        with open("auth.json", "r") as f:  # {"api_id": {{}},"api_hash": "{{}}","bot_token": "{{}}"}
-            auth = json.load(f)
-    else:
-        logger.debug("Authentication from env values")
-        has_string_session = "STRING_SESSION" in os.environ
-        auth = {
-            "api_id": int(os.environ.get("API_ID", 'wrong-ip')),
-            "api_hash": os.environ.get("API_HASH", 'wrong-hash'),
-        }
+def main():
+    auth = {
+        "session": StringSession(os.environ.get("STRING_SESSION")),
+        "api_id": int(os.environ.get("API_ID")),
+        "api_hash": os.environ.get("API_HASH"),
+    }
 
-    name = "worker"
-    api_id = auth["api_id"]
-    api_hash = auth["api_hash"]
-
-    client = TelegramClient(StringSession(os.environ.get("STRING_SESSION")) if has_string_session else StringSession(), api_id, api_hash)
-
-    with client:
-        if DEBUG: logger.debug(client.session.save())
-        client.loop.run_until_complete(main(client))
+    with TelegramClient(**auth) as client:
+        client.loop.run_until_complete(start(client))
         client.run_until_disconnected()
+
+
+if __name__ == '__main__':
+    main()
